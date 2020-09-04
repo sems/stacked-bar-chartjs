13b8329836147bfb13a4293b0357d6ce12ee7970
Sem Spanhaak <me@sems.dev>
2020-09-04 09:27:53 +0200

    Added a new bar

--- index.php
+++ index.php
@@ -14,8 +14,9 @@
 </head>
 
 <body>
-    <div style="width: 40%;">
-        <canvas id="myChart" width="400" height="400"></canvas>
+    <div style="width: 45%;">
+        <canvas id="pers" width="400" height="400"></canvas>
+        <canvas id="cap" width="400" height="400"></canvas>
     </div>
     <script>
         function getRandomInt(max) {
@@ -24,38 +25,70 @@
 
         var set = {
             negative: [
-                -.5,
-                getRandomInt(-14, 0),
-                getRandomInt(-14, 0),
-                getRandomInt(-14, 0),
-                getRandomInt(-14, 0),
-                getRandomInt(-14, 0),
-                getRandomInt(-14, 0),
-                getRandomInt(-14, 0),
-                getRandomInt(-14, 0),
-                getRandomInt(-14, 0),
-                getRandomInt(-14, 0),
-                getRandomInt(-14, 0),
-                getRandomInt(-14, 0),
-                getRandomInt(-14, 0),
+                getRandomInt(-12, 0),
+                getRandomInt(-12, 0),
+                getRandomInt(-12, 0),
+                getRandomInt(-12, 0),
+                getRandomInt(-12, 0),
+                getRandomInt(-12, 0),
+                getRandomInt(-12, 0),
+                getRandomInt(-12, 0),
+                getRandomInt(-12, 0),
+                getRandomInt(-12, 0),
+                getRandomInt(-12, 0),
+                getRandomInt(-12, 0),
+                getRandomInt(-12, 0),
+                getRandomInt(-12, 0),
             ],
             positive: [
-                .5,
-                getRandomInt(14),
-                getRandomInt(14),
-                getRandomInt(14),
-                getRandomInt(14),
-                getRandomInt(14),
-                getRandomInt(14),
-                getRandomInt(14),
-                getRandomInt(14),
-                getRandomInt(14),
-                getRandomInt(14),
-                getRandomInt(14),
-                getRandomInt(14),
-                getRandomInt(14),
+                getRandomInt(12),
+                getRandomInt(12),
+                getRandomInt(12),
+                getRandomInt(12),
+                getRandomInt(12),
+                getRandomInt(12),
+                getRandomInt(12),
+                getRandomInt(12),
+                getRandomInt(12),
+                getRandomInt(12),
+                getRandomInt(12),
+                getRandomInt(12),
+                getRandomInt(12),
+                getRandomInt(12),
             ],
-            colors: [
+            baseNegative: [
+                -.5,
+                -.5,
+                -.5,
+                -.5,
+                -.5,
+                -.5,
+                -.5,
+                -.5,
+                -.5,
+                -.5,
+                -.5,
+                -.5,
+                -.5,
+                -.5,
+            ],
+            basePositive: [
+                .5,
+                .5,
+                .5,
+                .5,
+                .5,
+                .5,
+                .5,
+                .5,
+                .5,
+                .5,
+                .5,
+                .5,
+                .5,
+                .5,
+            ],
+            colours: [
                 'rgba(247, 210, 52, 1)',
                 'rgba(247, 210, 52, 1)',
                 'rgba(247, 210, 52, 1)',
@@ -70,16 +103,23 @@
                 'rgba(237, 95, 24, 1)',
                 'rgba(237, 95, 24, 1)',
                 'rgba(237, 95, 24, 1)',
+            ],
+            horizontalColours: [
+                'rgba(255, 231, 6, 0.8)',
+                'rgba(39, 119, 204, .8)',
+                'rgba(255, 35, 33, .8)',
             ]
         }
 
-        var myChartData = {
+
+
+        var perTestData = {
             labels: ['Flexibiliteit', 'Onafhankelijkheid', 'Varieteit', 'Emontionele Controle', 'Doelgerichtheid', 'Punctualiteit', 'Conformiteit', 'Dilemma', 'Sociabileit', 'Altruisme', 'Overtuigingskracht', 'Prestatiemotivatie', 'Erkenning', 'Leiderschap'],
             datasets: [{
                     label: 'Min',
                     stack: 1,
-                    order: 0,
-                    backgroundColor: set.colors,
+                    order: 3,
+                    backgroundColor: set.colours,
                     data: set.negative,
                     barPercentage: 0.6,
                     categoryPercentage: 1,
@@ -87,20 +127,65 @@
                 {
                     label: 'Max',
                     stack: 1,
-                    order: 1,
-                    backgroundColor: set.colors,
+                    order: 2,
+                    backgroundColor: set.colours,
                     data: set.positive,
                     barPercentage: 0.6,
                     categoryPercentage: 1,
                 },
+                // {
+                //     label: '',
+                //     stack: 1,
+                //     order: 1,
+                //     backgroundColor: set.colours,
+                //     borderColor: 'rgba(255,255,255,0.0)',
+                //     borderSkipped: 'false',
+                //     data: set.baseNegative,
+                //     barPercentage: 1,
+                // },
+                // {
+                //     label: '',
+                //     stack: 1,
+                //     order: 0,
+                //     backgroundColor: set.colors,
+                //     borderColor: 'rgba(255,255,255,0.0)',
+                //     borderSkipped: 'false',
+                //     data: set.basePositive,
+                //     barPercentage: 1,
+                // }
             ]
         };
 
+        var capTestData = {
+            labels: ['Abstract', 'Numeriek', 'Verbaal'],
+            datasets: [{
+                label: 'Aantal goed',
+                order: 0,
+                data: [
+                    5,
+                    15,
+                    13,
+                ],
+                backgroundColor: set.horizontalColours
+            }, {
+                label: 'Aantal vragen',
+                order: 0,
+                data: [
+                    1,
+                    0,
+                    2,
+                ],
+                borderColor: set.horizontalColours,
+                borderWidth: 2,
+                backgroundColor: 'rgba(255,255,255,0.1)'
+            }]
+        }
+
         window.onload = function() {
-            var ctx = document.getElementById('myChart').getContext('2d');
-            window.myBar = new Chart(ctx, {
+            var ctx = document.getElementById('pers').getContext('2d');
+            window.persTestBar = new Chart(ctx, {
                 type: 'bar',
-                data: myChartData,
+                data: perTestData,
                 options: {
                     tooltips: {
                         mode: 'index',
@@ -123,11 +208,53 @@
                             ticks: {
                                 maxTicksLimit: 30,
                                 suggestedMax: 15
-                            }
+                            },
                         }]
                     }
                 }
             });
+
+            // let ScaleCapaciteit = Chart.Scale.extend({
+            //     left: 0,
+            //     right: 15
+            // });
+
+            // Chart.scaleService.registerScaleType('ScaleCapaciteit', ScaleCapaciteit, defaultConfigObject);
+
+            var rtx = document.getElementById('cap').getContext('2d');
+            window.capTestBar = new Chart(rtx, {
+                type: 'horizontalBar',
+                data: capTestData,
+                options: {
+                    legend: {
+                        display: true,
+                        labels: {
+                            fontColor: 'rgb(255, 99, 132)'
+                        }
+                    },
+                    tooltips: {
+                        mode: 'index',
+                        intersect: false
+                    },
+                    responsive: true,
+                    scales: {
+                        xAxes: [{
+                            stacked: true,
+                            // type: 'ScaleCapaciteit'
+                        }],
+                        yAxes: [{
+                            stacked: true,
+                            ticks: {
+                                maxTicksLimit: 15,
+                                suggestedMax: 15
+                            },
+                            gridLines: {
+                                drawOnChartArea: false, // only want the grid lines for one axis to show up
+                            },
+                        }]
+                    }
+                }
+            })
         };
     </script>
 </body>
